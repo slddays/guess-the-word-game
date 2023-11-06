c2665aeae5129d9d8277eb68e30b89f2bfb98e97
slddays <kimhnh@gmail.com>
2023-11-06 17:52:30 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/*TODO LIST:
+array of words you can shuffle through (for example 5 words)
+scramble the letters of a word
+each letter has a span which is then shown in .display
+logic: 5 tries --> u fail if it hits 5 (tries)
+logic: matches if a letter is in the right spot (mistakes)
+*/
