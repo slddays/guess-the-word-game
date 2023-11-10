1ecf5c1c303b5bdea615afb61484f90b94525c60
slddays <kimhnh@gmail.com>
2023-11-11 00:06:33 +0900

    fix input logic

--- app.js
+++ app.js
@@ -1,112 +1,121 @@
-/* TODO LIST:
-fix input logic. pushing to new word array + other things are breaking
-*/
+document.addEventListener('DOMContentLoaded', () => {
+  const display = document.querySelector('.display');
+  const input = document.querySelector('.input');
+  const tryText = document.querySelector('.try');
+  const mistakeText = document.querySelector('.mistake');
+  const allCircles = document.querySelectorAll('.step-circle');
+  const randomBtn = document.getElementById('randomBtn');
+  const resetBtn = document.getElementById('resetBtn');
 
-const display = document.querySelector('.display');
-const input = document.querySelector('.input');
-const tryText = document.querySelector('.try');
-const mistakeText = document.querySelector('.mistake');
-const allCircles = document.querySelectorAll('.step-circle');
-const randomBtn = document.getElementById('randomBtn');
-const resetBtn = document.getElementById('resetBtn');
+  const list = ['flower', 'valley', 'lake', 'meadow', 'ocean']; // hard-coded list
+  let tries = 0;
+  let circleIndex = 0;
+  let finalAnswer = [];
 
-const list = ['flower', 'valley', 'lake', 'meadow']; // hard-coded list
-let tries = 0;
+  randomBtn.addEventListener('click', grabWord);
+  resetBtn.addEventListener('click', removeContent);
 
-randomBtn.addEventListener('click', grabWord);
-resetBtn.addEventListener('click', removeContent); // how to delete input text?
-
-function grabWord() {
-  display.innerHTML = '';
-  input.innerHTML = '';
-  removeContent();
-  showPrompt();
-}
-
-function showPrompt() {
-  const word = list[randomNum(list.length)];
-  const answer = word.split('');
-  const shuffleWord = word.shuffle();
-
-  for (let i = 0; i < shuffleWord.length; i++) {
-    // Create span
-    const letterDisplay = document.createElement('span');
-    letterDisplay.textContent = shuffleWord[i];
-    letterDisplay.classList.add('letter-display');
-    display.appendChild(letterDisplay);
-
-    // Create input
-    const letterInput = document.createElement('input');
-    letterInput.classList.add('letter-input');
-    letterInput.setAttribute('id', `input${i}`);
-    letterInput.type = 'text';
-    letterInput.maxLength = '1';
-    letterInput.value = '';
-    input.appendChild(letterInput);
+  function grabWord() {
+    display.innerHTML = '';
+    input.innerHTML = '';
+    removeContent();
+    showPrompt();
   }
 
-  const finalAnswer = [];
-  const allInputs = document.querySelectorAll('input');
-  allInputs.forEach((input, index) => {
-    input.addEventListener('input', (e) => {
-      input.textContent = e.target.value;
-      input.setAttribute('value', `${e.target.value}`);
-      finalAnswer.push(document.getElementById(`input${index}`).value); //push each input into a new array
+  function showPrompt() {
+    const word = list[randomNum(list.length)];
+    const answer = word.split('');
+    const shuffleWord = word.shuffle();
 
-      let letters = /^[a-zA-Z]+$/;
-      const nextInputIndex = index + 1;
-      if (input.value.match(letters) && nextInputIndex < allInputs.length) {
-        document.getElementById(`input${nextInputIndex}`).focus();
-      } else {
-        document.activeElement.blur();
-      }
+    for (let i = 0; i < shuffleWord.length; i++) {
+      // create span
+      const letterDisplay = document.createElement('span');
+      letterDisplay.textContent = shuffleWord[i];
+      letterDisplay.classList.add('letter-display');
+      display.appendChild(letterDisplay);
 
-      tries++;
-      if (input.value !== answer[index] && tries < 6) {
-        tryText.textContent = `Tries (${tries}/5):`;
-        mistakeText.textContent += `${input.value}, `;
-        document.getElementById(`step${tries}`).classList.add('completed');
-      } else if (tries > 7) {
-        alert('😭 Game over...');
-        grabWord();
-      } else if (
-        input.value === answer[index] &&
-        tries < 6 &&
-        answer.join('') === finalAnswer.join('')
-      ) {
-        alert('🎉 Success!');
-        grabWord();
-      }
+      // create input
+      const letterInput = document.createElement('input');
+      letterInput.classList.add('letter-input');
+      letterInput.setAttribute('id', `input${i}`);
+      letterInput.type = 'text';
+      letterInput.maxLength = '1';
+      letterInput.value = '';
+      input.appendChild(letterInput);
+    }
+
+    const allInputs = document.querySelectorAll('input');
+    allInputs.forEach((input, index) => {
+      input.addEventListener('input', (e) => {
+        input.textContent = e.target.value;
+        input.setAttribute('value', `${e.target.value}`);
+
+        // letter inputs + focus/blur
+        let letters = /^[a-zA-Z]+$/;
+        const nextInputIndex = index + 1;
+        if (input.value.match(letters) && nextInputIndex < allInputs.length) {
+          document.getElementById(`input${nextInputIndex}`).focus();
+        } else {
+          document.activeElement.blur();
+        }
+
+        // condition: correct inputs get pushed into new array
+        if (input.value === answer[index]) {
+          finalAnswer.push(document.getElementById(`input${index}`).value);
+        }
+
+        //condition: wrong inputs
+        if (input.value !== answer[index]) {
+          tries++;
+          circleIndex++;
+          if (tries < 6) {
+            tryText.textContent = `Tries (${tries}/5):`;
+            mistakeText.textContent += `${input.value}, `;
+            document.getElementById(`step${circleIndex}`).classList.add('completed');
+          } else if (tries === 6) {
+            alert('😭 Game over...');
+            grabWord();
+          }
+        }
+
+        // condition: solved
+        if (answer.join('') === finalAnswer.join('')) {
+          alert('🎉 Success!');
+          grabWord();
+        }
+      });
     });
-  });
-}
-
-String.prototype.shuffle = function () {
-  const a = this.split(''), //split string into array
-    n = a.length; // array length
-
-  for (let i = n - 1; i > 0; i--) {
-    //cycle through every index
-    const k = Math.floor(Math.random() * (i + 1));
-    const tmp = a[i];
-    a[i] = a[k];
-    a[k] = tmp;
   }
-  return a.join('');
-};
 
-function randomNum(max) {
-  return Math.floor(Math.random() * max);
-}
+  // fisher-yates shuffle
+  String.prototype.shuffle = function () {
+    const a = this.split(''),
+      n = a.length;
 
-function removeContent() {
-  tries = 0;
-  tryText.textContent = 'Tries (0/5):';
-  allCircles.forEach((el) => {
-    el.classList.remove('completed');
-  });
-  mistakeText.textContent = '';
-}
+    for (let i = n - 1; i > 0; i--) {
+      const k = Math.floor(Math.random() * (i + 1));
+      const tmp = a[i];
+      a[i] = a[k];
+      a[k] = tmp;
+    }
+    return a.join('');
+  };
 
-//fire up a random word when page loads
-showPrompt();
+  function randomNum(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function removeContent() {
+    tries = 0;
+    circleIndex = 0;
+    finalAnswer = [];
+    tryText.textContent = 'Tries (0/5):';
+    allCircles.forEach((el) => {
+      el.classList.remove('completed');
+    });
+    mistakeText.textContent = '';
+  }
+
+  // fire up random word when page loads
+  showPrompt();
+});
